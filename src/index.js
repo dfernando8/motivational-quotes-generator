1c0cab17668a0bc0bdc997fdcaca717f7722b7e1
dfernando8 <151517750+dfernando8@users.noreply.github.com>
2024-02-18 20:13:58 +0000

    Added SheCodes AI API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-function generateQuote(event) {
-  event.preventDefault();
-
+function showQuote(response) {
   new Typewriter("#quote", {
-    strings:
-      "Nothing has happened in the past; it happened in the Now. Nothing will ever happen in the future; it will happen in the Now.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 40,
     cursor: null,
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instruction");
+  let apiKey = "0c9a9eb6fa633t1c3o446ff55ab61320";
+  let context =
+    "You are a motivational AI assistant and provide short motiviational quotes from the book Power of Now. Make sure the quotes are simple and short. Do not include the authors name";
+  let prompt = `Generate a motivational quote about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let quoteElement = document.querySelector("#quote");
+
+  quoteElement.innerHTML = "Generating quote...";
+  console.log("Generating quote...");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(showQuote);
 }
 
 let quoteFormElement = document.querySelector("#quotes-generator-form");
